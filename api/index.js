11f51ec2356f5289bc2a1e93ce4626c9b91e8b98
GODWIN <godwindass14@gmail.com>
2024-05-28 13:35:13 +0530

    create test and sign up api route

--- api/index.js
+++ api/index.js
@@ -1,18 +1,28 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
-dotenv.config()
+dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(
     () =>{
-        console.log('connected to mongo db')
+        console.log('connected to mongo db');
     }
 ).catch( (err) =>{
-    console.log(err)
-})
-const app = express()
+    console.log(err);
+});
+
+
+
+const app = express();
 
 app.listen(3000, () =>{
     console.log("listening in 3000..")
-})
+});
+
+app.use(express.json());
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
