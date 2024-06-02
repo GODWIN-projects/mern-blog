2e50f755673c013d735b91498f2dc3c5be1e89f4
GODWIN <godwindass14@gmail.com>
2024-06-02 14:40:39 +0530

    updated header to show current user

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,13 @@
-import { Button, Navbar, TextInput } from 'flowbite-react'
+import { Avatar, Button, Dropdown, DropdownHeader, DropdownItem, Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai'
 import {FaMoon} from 'react-icons/fa'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
     const path = useLocation().pathname
+    const { currentUser } = useSelector(state => state.user)
   return (
     <Navbar className=' border-b-2'>
         <Link to="/" className=' self-center whitespace-nowrap
@@ -29,12 +31,35 @@ const Header = () => {
             <Button className='w-12 hidden sm:inline ' color="gray" pill>
                 <FaMoon />
             </Button>
+            { currentUser? (
+                <Dropdown
+                    arrowIcon={false}
+                    inline
+                    label= {
+                        <Avatar
+                            alt='user'
+                            img={currentUser.photoURL}
+                            rounded/>
+                    }>
+                        <DropdownHeader>
+                            <span className='block text-sm'>{currentUser.username}</span>
+                            <span className='block text-sm font-medium truncate'>{currentUser.email} </span>
+                        </DropdownHeader>
+                        <Link to='/dashboard?tab=profile'>
+                            <Dropdown.Item>Profile</Dropdown.Item>
+                        </Link>
+                        <Dropdown.Divider/>
+                        <Dropdown.Item>Sign Out</Dropdown.Item>
+                </Dropdown>
+            ):
+            (
             <Link to='/sign-in'>
-                <Button className='border-0 rounded-lg
-             text-black font-semibold' outline gradientDuoTone='tealToLime'>
+                <Button outline gradientDuoTone='tealToLime'>
                     Sign in
                 </Button>
             </Link>
+            )
+            }
             <Navbar.Toggle />
         </div>
         <Navbar.Collapse>
